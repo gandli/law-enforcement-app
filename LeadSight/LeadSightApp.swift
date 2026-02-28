2e3069f5abbe3d815dc3817774a29a16b52c1f1f
gandli <gandli@qq.com>
2026-03-01 00:03:48 +0800

    Initial Commit

---
+++ LeadSight/LeadSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeadSightApp.swift
+//  LeadSight
+//
+//  Created by user on 2026/3/1.
+//
+
+import SwiftUI
+
+@main
+struct LeadSightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
